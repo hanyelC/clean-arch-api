0856b57e9f0718f40a3caffdb18bae94f9f2503b
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-20 15:18:35 -0300

    return void instead of Promise

--- src/main/config/apollo-server.ts
+++ src/main/config/apollo-server.ts
@@ -4,13 +4,13 @@ import resolvers from '@/main/graphql/resolvers'
 import { ApolloServer } from 'apollo-server-express'
 import { type Express } from 'express'
 
-export const setupApolloServer = async (app: Express): Promise<void> => {
+export const setupApolloServer = (app: Express): void => {
   const server = new ApolloServer({
     resolvers,
     typeDefs
   })
 
-  await server.start()
-
-  server.applyMiddleware({ app })
+  server.start().then(_ => {
+    server.applyMiddleware({ app })
+  }).catch(_ => { /**/ })
 }

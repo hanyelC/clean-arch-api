42f266c8f746741244096fb395579067022350d9
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-20 15:08:08 -0300

    add Express router adapter

---
+++ src/main/adapters/express-router.ts
@@ -0,0 +1,9 @@
+import { type Controller } from '@/presentation/contracts'
+import { type Request, type Response } from 'express'
+
+export const adaptRoute = (controller: Controller) => {
+  return async (req: Request, res: Response) => {
+    const httpResponse = await controller.handle()
+    return res.status(httpResponse.statusCode).json(httpResponse.data)
+  }
+}

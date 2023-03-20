a3e784c4952f164489df4ef5324a3e178a5dc576
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-20 15:13:23 -0300

    add ranking graphql queries

---
+++ src/main/config/apollo-server.ts
@@ -0,0 +1,16 @@
+import typeDefs from '@/main/graphql/type-defs'
+import resolvers from '@/main/graphql/resolvers'
+
+import { ApolloServer } from 'apollo-server-express'
+import { type Express } from 'express'
+
+export const setupApolloServer = async (app: Express): Promise<void> => {
+  const server = new ApolloServer({
+    resolvers,
+    typeDefs
+  })
+
+  await server.start()
+
+  server.applyMiddleware({ app })
+}
